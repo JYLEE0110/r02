edafc93fe2868304657d692963442115abb9f8e0
JYLEE0110 <wndyd0110@gmail.com>
2023-07-20 11:51:11 +0900

    PracticeReactSilentRefresh

--- src/api/productAPI.js
+++ src/api/productAPI.js
@@ -1,12 +1,13 @@
-import axios from "axios"
+
 import { createSearchParams } from "react-router-dom";
+import jwtAxios from "../util/jwtUtil";
 
 export const getList = async(queryObj) => {
 
     // Json 데이터를 쿼리스트링으로 변경
     const queryString = createSearchParams(queryObj).toString()
 
-    const res = await axios.get(`http://localhost:8080/api/products/list?${queryString}`);
+    const res = await jwtAxios.get(`http://localhost:8080/api/products/list?${queryString}`);
 
     return res.data
 }
@@ -20,14 +21,14 @@ export const postProduct = async (formData) => {
         }
     }
 
-    const res = await axios.post(`http://localhost:8080/api/products/`, formData, header)
+    const res = await jwtAxios.post(`http://localhost:8080/api/products/`, formData, header)
 
     return res.data
 }
 
 export const getProduct = async(pno) => {
 
-    const res = await axios.get(`http://localhost:8080/api/products/${pno}`)
+    const res = await jwtAxios.get(`http://localhost:8080/api/products/${pno}`)
 
     return res.data
 
@@ -35,7 +36,7 @@ export const getProduct = async(pno) => {
 
 export const deleteProduct = async(pno) => {
 
-    const res = await axios.delete(`http://localhost:8080/api/products/${pno}`)
+    const res = await jwtAxios.delete(`http://localhost:8080/api/products/${pno}`)
 
     return res.data
 
@@ -49,7 +50,7 @@ export const putProduct = async (formData) => {
         }
     }
 
-    const res = await axios.post(`http://localhost:8080/api/products/modify`, formData, header)
+    const res = await jwtAxios.post(`http://localhost:8080/api/products/modify`, formData, header)
 
     return res.data
 }
